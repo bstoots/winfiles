ddadbb14da1aacc675a519ed92f5896a1c260a26
Brian Stoots <bstoots@gmail.com>
2016-04-03 01:36:21 -0400

    Fix clink path

--- .cmdrc.bat
+++ .cmdrc.bat
@@ -4,4 +4,4 @@
 doskey.exe cd=pushd $*
 
 :: Inject clink into all running CMDs (https://mridgers.github.io/clink/)
-call %CLINK_DIR%\clink.bat inject --autorun --profile ~\clink
+call "%CLINK_DIR%\clink.bat" inject --autorun --profile ~\clink
